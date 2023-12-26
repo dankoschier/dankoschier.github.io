55951060632f2e9269696d2d823f662ab1b91387
Dan Alexander Koschier <dan.koschier@gmail.com>
2023-12-26 11:22:54 +0100

    Update location

--- js/maps.js
+++ js/maps.js
@@ -2,7 +2,7 @@ function initMap() {
     var map_div = document.getElementById('id_map');
     var pos = {
         lat: 51.5371756,
-        lng: -0.1290173
+        lng: -0.126445
     };
 
     var map = new google.maps.Map(map_div, {
